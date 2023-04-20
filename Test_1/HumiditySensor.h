a2e2e0da81b64bdda2ce1cfa41608b4b40a359c4
Alexey Gr <alex_gr2@mail.ru>
2023-04-21 00:16:05 +0700

    fafasf

--- Test_1/HumiditySensor.h
+++ Test_1/HumiditySensor.h
@@ -1,34 +1,49 @@
 #pragma once
 #include "TemperatureSensorC0.h"
 
-// Датчик Влажности
-class HumiditySensor
-{
-private:
-	
-	// Средняя Влажность датчика
-	double AverageHumidity;
+// Абстрактный класс датчика влажности
+class HumiditySensor {
+protected:
+	double Value;
 
 public:
-	double GetHumidity();			// Получить влажность с датчика
-
-	HumiditySensor();
-
-	HumiditySensor(double humidity);
-
-	// Установить среднюю Владность датчика
-	void SetAverageHum(double humidity);
-
-	// Повысить влажность на 1 процент
-	void UpOneHimid();
-
-	// Понизить влажность на 1 процент
-	void DownOneHimid();
-
-protected:
-
-	// Сгенерировать влажность для имитации работы датчика
-	double GenericHumidity(double min, double max);
-
+	virtual double readHumidity() = 0;
+	virtual ~HumiditySensor() {}
 };
 
+// Реализация датчика влажности на основе сенсора 1
+class Sensor1 : public HumiditySensor {
+public:
+	double readHumidity() override {
+		// влажность с датчика 1
+		return 50.0; 
+	}
+};
+
+// Реализация датчика влажности на основе сенсора 2
+class Sensor2 : public HumiditySensor {
+public:
+	double readHumidity() override {
+		// влажность с датчика 2
+		return 60.0;
+	}
+};
+
+// Класс "комоновщик" датчиков влажности
+class HumiditySensors {
+public:
+	void addSensor(HumiditySensor* sensor) {
+		sensors.push_back(sensor);
+	}
+
+	double readHumidity() {
+		double totalHumidity = 0.0;
+		for (HumiditySensor* sensor : sensors) {
+			totalHumidity += sensor->readHumidity();
+		}
+		return totalHumidity / sensors.size();
+	}
+
+private:
+	std::vector<HumiditySensor*> sensors;
+};
